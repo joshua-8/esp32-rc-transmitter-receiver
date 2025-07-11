cb3d4d69b953bbf0ccf7814307a389dcff68fddc
joshua-8 <joshua262144@gmail.com>
2025-07-10 23:26:16 -0700

    joystick and servo

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,49 +1,89 @@
-#include <Arduino.h>
-#include <ESP32_easy_wifi_data.h>
+#include <Arduino.h> // tested with esp32 boards version 3.2.1
+#include <ESP32_easy_wifi_data.h> // tested with version 1.4.3
 /*
- * This example is a simple wifi controlled "robot" that turns on the built in led when told to.
- */
+   Creates wifi network
+   Outputs 4 servo signals
+*/
+
+const byte ch1Pin = 32; // speed_rc_pin
+const byte ch2Pin = 33; // turn_rc_pin
+const byte ch3Pin = 25; // stop_rc_pin
+const byte ch4Pin = 26; // ctrl_rc_pin
+
+int ch1Val;
+int ch2Val;
+int ch3Val;
+int ch4Val;
 
 const int ledPin = 2;
-const int buttonPin = 0;
-
-boolean ledVal = false;
-boolean buttonVal = false;
 
 // data receive callback
 void WifiDataToParse()
 {
-    ledVal = EWD::recvBl();
+  ch1Val = EWD::recvIn();
+  ch2Val = EWD::recvIn();
+  ch3Val = EWD::recvIn();
+  ch4Val = EWD::recvIn();
 }
 // data send callback
 void WifiDataToSend()
 {
-    EWD::sendBl(buttonVal);
+  EWD::sendBl(digitalRead(0)); // I think it needs something to be sent
 }
 
 void configWifi()
 {
-    EWD::mode = EWD::Mode::createAP;
-    EWD::APPort = 25001;
-    EWD::APName = "esptest1";
-    EWD::APPassword = "password";
+  EWD::mode = EWD::Mode::createAP;
+  EWD::APPort = 25001;
+  EWD::APName = "gbgCarDemo";
+  EWD::APPassword = "2521sert"; // no one will ever guess this
 }
 
 void setup()
 {
-    pinMode(ledPin, OUTPUT);
-    Serial.begin(115200);
-    configWifi();
-    EWD::setupWifi(WifiDataToParse, WifiDataToSend);
-    Serial.println("example_controlled running");
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
+  configWifi();
+  EWD::setupWifi(WifiDataToParse, WifiDataToSend);
+  Serial.println("receiver running");
+
+  ledcAttach(ch1Pin, 50, 14);
+  ledcAttach(ch2Pin, 50, 14);
+  ledcAttach(ch3Pin, 50, 14);
+  ledcAttach(ch4Pin, 50, 14);
 }
 void loop()
 {
-    buttonVal = digitalRead(buttonPin);
-    EWD::runWifiCommunication();
-    if (EWD::newData()) {
-        Serial.println("got new data");
-    }
-    digitalWrite(ledPin, ledVal);
-    delay(10);
+  EWD::runWifiCommunication();
+  if (EWD::newData()) {
+    digitalWrite(ledPin, HIGH);
+
+    //    Serial.print(ch1Val);
+    //    Serial.print(", ");
+    //    Serial.print(ch2Val);
+    //    Serial.print(", ");
+    //    Serial.print(ch3Val);
+    //    Serial.print(", ");
+    //    Serial.print(ch4Val);
+    //    Serial.println();
+
+    servoWriteMicroseconds(ch1Pin, ch1Val);
+    servoWriteMicroseconds(ch2Pin, ch2Val);
+    servoWriteMicroseconds(ch3Pin, ch3Val);
+    servoWriteMicroseconds(ch4Pin, ch4Val);
+  }
+  if (EWD::timedOut()) { // connection lost
+    digitalWrite(ledPin, LOW);
+    // set pins low to indicate no connection
+    ledcWrite(ch1Pin, 0);
+    ledcWrite(ch2Pin, 0);
+    ledcWrite(ch3Pin, 0);
+    ledcWrite(ch4Pin, 0);
+  }
+  delay(10);
+}
+
+bool servoWriteMicroseconds(uint8_t pin, int ms) {
+  ms = constrain(ms, 1000, 2000);
+  return ledcWrite(pin, ms * 512 / 624);
 }
--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,51 +1,118 @@
-#include <ESP32_easy_wifi_data.h>
+#include <Arduino.h> // tested with esp32 boards version 3.2.1
+#include <ESP32_easy_wifi_data.h> // tested with version 1.4.3
 /*
- * This example is a simple controller that sends data to the example_controlled example of whether the built in button is pressed
- */
+   Connects to a wifi network
+   Reads a joystick and sends 4 channels of information
+*/
 
 const int ledPin = 2;
 const int buttonPin = 0;
 
-boolean buttonVal = false;
-boolean ledVal = false;
+const byte ch1Pin = 34; // forwards joystick axis
+const byte ch2Pin = 35; // right joystick axis
+const byte ch3Pin = 26; // stop switch, connect to ground to turn off
+const byte ch4Pin = 27; // who has control switch, connect to ground to turn off override
+
+#define CH1CONFIG 50, 1640, 3770, 100 // back, center, forwards, deadzone
+#define CH2CONFIG 4080, 1973, 100, 100 // left, center, right, deadzone
+
+int ch1Val;
+int ch2Val;
+int ch3Val;
+int ch4Val;
 
 // data receive callback
 void WifiDataToParse()
 {
-    ledVal = EWD::recvBl();
+  digitalWrite(ledPin, EWD::recvBl());
 }
 // data send callback
 void WifiDataToSend()
 {
-    EWD::sendBl(buttonVal);
+  EWD::sendIn(ch1Val);
+  EWD::sendIn(ch2Val);
+  EWD::sendIn(ch3Val);
+  EWD::sendIn(ch4Val);
 }
 
 void configWifi()
 {
-    EWD::mode = EWD::Mode::connectToNetwork;
-    EWD::routerName = "esptest1";
-    EWD::routerPassword = "password";
-    EWD::routerPort = 25001;
-    EWD::communicateWithIP = "192.168.4.1";
-    EWD::debugPrint = true;
+  EWD::mode = EWD::Mode::connectToNetwork;
+  EWD::routerName = "gbgCarDemo";
+  EWD::routerPassword = "2521sert"; // no one will ever guess this
+  EWD::routerPort = 25001;
+  EWD::communicateWithIP = "192.168.4.1"; // the address the receiver gives itself in AP mode
+  EWD::debugPrint = true;
 }
 
 void setup()
 {
-    pinMode(ledPin, OUTPUT);
-    Serial.begin(115200);
-    configWifi();
-    EWD::setupWifi(WifiDataToParse, WifiDataToSend);
-    Serial.println("controller running");
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
+  configWifi();
+  EWD::setupWifi(WifiDataToParse, WifiDataToSend);
+  Serial.println("controller running");
+
+  pinMode(ch3Pin, INPUT_PULLUP);
+  pinMode(ch4Pin, INPUT_PULLUP);
 }
 
 void loop()
 {
-    buttonVal = digitalRead(buttonPin);
-    EWD::runWifiCommunication();
-    if (EWD::newData()) {
-        Serial.println("got new data");
-    }
-    digitalWrite(ledPin, ledVal);
-    delay(10);
+  float ch1 = InputReader_JoystickAxis(analogRead(ch1Pin), CH1CONFIG);
+  float ch2 = InputReader_JoystickAxis(analogRead(ch2Pin), CH2CONFIG);
+  float ch3 = digitalRead(ch3Pin) ? 1 : -1;
+  float ch4 = digitalRead(ch4Pin) ? 1 : -1;
+
+  ch1Val = ch1 * 500 + 1500;
+  ch2Val = ch2 * 500 + 1500;
+  ch3Val = ch3 * 500 + 1500;
+  ch4Val = ch4 * 500 + 1500;
+
+  //  Serial.print(analogRead(ch1Pin));
+  //  Serial.print(" ");
+  //  Serial.print(ch1Val);
+  //  Serial.print(",    ");
+  //  Serial.print(analogRead(ch2Pin));
+  //  Serial.print(" ");
+  //  Serial.print(ch2Val);
+  //  Serial.print(",    ");
+  //  Serial.print((ch3));
+  //  Serial.print(" ");
+  //  Serial.print(ch3Val);
+  //  Serial.print(",    ");
+  //  Serial.print((ch4));
+  //  Serial.print(" ");
+  //  Serial.print(ch4Val);
+  //  Serial.println();
+
+  EWD::runWifiCommunication();
+  if (EWD::timedOut()) {
+    digitalWrite(ledPin, LOW);
+  }
+
+  delay(10);
+}
+
+float InputReader_JoystickAxis(int reading, int neg, int cent, int pos, int deadzone) {
+  deadzone = max(deadzone, 0);
+  ////////// scale joystick values from adjustible range to -1 to 1
+  float output = 0;
+  if (neg < pos) {  //if the value for when the joystick is towards the left is less then the value for when the joystick is towards the right...
+    if (reading < cent - deadzone && neg < cent - deadzone)
+      output = floatMap(reading, neg, cent - deadzone, -1, 0);
+    if (reading > cent + deadzone && pos > cent + deadzone)
+      output = floatMap(reading, pos, cent + deadzone, 1, 0);
+  }
+  if (neg > pos) {  // joystick axis is backwards
+    if (reading > cent + deadzone && neg > cent + deadzone)
+      output = floatMap(reading, neg, cent + deadzone, -1, 0);
+    if (reading < cent - deadzone && pos < cent - deadzone)
+      output = floatMap(reading, pos, cent - deadzone, 1, 0);
+  }
+  return constrain(output, -1, 1);
+}
+
+float floatMap(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
